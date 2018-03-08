b18e0e9cb44ebacfbf7992a48f156a396a5cf543
Rohith <gambol99@gmail.com>
2018-03-08 22:08:19 +0000

    - updating the outputs for the module

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,15 @@
-#output "dns" {
-#  description = "The FQDN of the newly created ELB"
-#  value       = "${var.dns_name}.${var.dns_zone}"
-#}
-#
-#output "alb_id" {
-#  description = "The ID for the ELB which has been created"
-#  value       = "${aws_lb.balancer.id}"
-#}
-#
-#output "alb_dns_name" {
-#  description = "The name given to the ELB just created"
-#  value       = "${aws_lb.balancer.dns_name}"
-#}
-#
-#output "security_group_id" {
-#  description = "The ID for the security used to protected the NLB"
-#  value       = "${aws_security_group.sg.id}"
-#}
+output "dns" {
+  description = "The FQDN of the newly created ELB"
+  value       = "${var.dns_name}.${var.dns_zone}"
+}
+
+output "nlb_id" {
+  description = "The ID for the ELB which has been created"
+  value       = "${aws_lb.balancer.id}"
+}
+
+output "nlb_dns_name" {
+  description = "The name given to the ELB just created"
+  value       = "${aws_lb.balancer.dns_name}"
+}
 
