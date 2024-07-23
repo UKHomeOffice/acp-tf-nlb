ba7014fe5c85bfb1f871f65f0a79b9d618a07cf9
Kabir Mahmood <93136759+kmahmood2021@users.noreply.github.com>
2024-07-23 14:26:12 +0100

    Updated terraform module to enable support for subnet mappings for in… (#15)
    * Updated terraform module to enable support for subnet mappings for internal NLB
    
    * ACPENG-2141: add foreach to add ingress security groups for each port (#16)
    
    * ACPENG-2141: add foreach to add ingress security groups for each port
    
    * ACPENG-2141: add foreach to add ingress security groups for each port
    
    * ACPENG-2141: add foreach to add ingress security groups for each port
    
    * ACPENG-2141: add foreach to add ingress security groups for each port
    
    * ACPENG-2141: add foreach to add ingress security groups for each port
    
    * ACPENG-2141: add foreach to add ingress security groups for each port
    
    * ACPENG-2141: terraform fmt
    
    * ACPENG-2141: terraform docs
    
    ---------
    
    Co-authored-by: Kabir Mahmood <kabir.mahmood@digital.homeoffice.gov.uk>
    Co-authored-by: Johnny Che <114401755+chej-hod@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -19,10 +19,14 @@ resource "aws_security_group" "balancer" {
 }
 
 resource "aws_security_group_rule" "ingress" {
-  count             = var.disable_security_groups ? 0 : 1
+  for_each = {
+    for key, value in var.ports : key => value
+    if var.disable_security_groups == false
+  }
+
   type              = "ingress"
-  from_port         = 0
-  to_port           = 0
+  from_port         = each.key
+  to_port           = each.key
   protocol          = "tcp"
   cidr_blocks       = var.security_group_ingress_cidr
   security_group_id = aws_security_group.balancer[0].id
@@ -119,10 +123,19 @@ resource "aws_lb" "balancer" {
   name = "${var.environment}-${var.name}-nlb"
 
   enable_cross_zone_load_balancing = "true"
-  internal                         = var.internal
+  internal                         = length(var.internal_nlb_subnet_mappings) > 0 ? true : var.internal
   load_balancer_type               = "network"
-  subnets                          = var.subnet_ids
-  security_groups                  = var.disable_security_groups ? null : [aws_security_group.balancer[0].id] # Disable for backwards compatability with version 2 of this module
+  subnets                          = length(var.internal_nlb_subnet_mappings) > 0 ? null : var.subnet_ids
+
+  dynamic "subnet_mapping" {
+    for_each = var.internal_nlb_subnet_mappings
+    content {
+      subnet_id            = subnet_mapping.value.subnet_id
+      private_ipv4_address = subnet_mapping.value.private_ipv4_address
+    }
+  }
+
+  security_groups = var.disable_security_groups ? null : [aws_security_group.balancer[0].id] # Disable for backwards compatability with version 2 of this module
 
   tags = merge(
     var.tags,
